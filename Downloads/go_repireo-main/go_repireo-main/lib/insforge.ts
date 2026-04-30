c810aa9776058f9fc86991eca98947e99f108e41
Chinmaya24 <chinmaikn@gmail.com>
2026-04-30 22:05:44 +0530

    Delete Downloads/go_repireo-main/go_repireo-main directory

--- Downloads/go_repireo-main/go_repireo-main/lib/insforge.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@insforge/sdk';
-
-export const insforge = createClient({
-  baseUrl: 'https://xipxmg4q.us-west.insforge.app',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3OC0xMjM0LTU2NzgtOTBhYi1jZGVmMTIzNDU2NzgiLCJlbWFpbCI6ImFub25AaW5zZm9yZ2UuY29tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MTc3MjJ9.pLCg4auhgJlP4VMALYDIj8BwgG6-14Hf7U3y-Ruvytc'
-});
